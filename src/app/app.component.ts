2284cf3a8a1d43503c02ab0dafbd9314f68bae22
STUNC <tunc0687@hotmail.com>
2020-05-22 18:42:09 +0300

    Uygulama tasarımı yapıldı

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoApp';
+    user = 'Duygu';
+    items = [
+        {description: "Spor", action: false},
+        {description: "Kitap Okumak", action: false},
+        {description: "Araştırma yapmak", action: false},
+        {description: "Dil Öğrenmek", action: false},
+    ];
 }
